c160efd925392fcfbb0ef139e904429157aab783
jbjagani <jayshinhvavdi00@gmail.com>
2025-06-14 11:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Optional future interaction scripts
 console.log("JB Jagani Portfolio Loaded.");
+// Script to control background music
+const bgMusic = document.getElementById("bgMusic");
+const playBtn = document.getElementById("playBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+
+playBtn.addEventListener("click", () => {
+  bgMusic.play();
+});
+
+pauseBtn.addEventListener("click", () => {
+  bgMusic.pause();
+});
